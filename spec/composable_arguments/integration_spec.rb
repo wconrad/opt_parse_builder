2315db5136f074a52a3ecbad19f75c42f7da6734
Wayne Conrad <kf7qga@gmail.com>
2020-11-21 08:03:39 -0700

    Test option with value

--- spec/composable_arguments/integration_spec.rb
+++ spec/composable_arguments/integration_spec.rb
@@ -107,7 +107,79 @@ describe "integration tests" do
     
   end
 
-  describe "option with value" do
+  describe "Option with value, no default" do
+
+    let(:args) do
+      args = ComposableArguments.new
+      args.add do |arg|
+        arg.key :foo
+        arg.on "--foo=VALUE", "--foo", "Set foo to VALUE"
+      end
+      args
+    end
+
+    it "prints help" do
+      test_harness.args = args
+      test_harness.parse!(["-h"])
+      expect(test_harness.output).to eq <<~OUTPUT
+        Usage: rspec [options]
+                --foo=VALUE                  Set foo to VALUE
+        (exit 0)
+      OUTPUT
+    end
+
+    it "is nil before parsing" do
+      expect(args[:foo]).to be_nil
+    end
+
+    it "is nil after parsing when not set" do
+      args.parse!([])
+      expect(args[:foo]).to be_nil
+    end
+
+    it "is the appropriate value when set" do
+      args.parse!(["--foo=BAR"])
+      expect(args[:foo]).to eq "BAR"
+    end
+    
+  end
+
+  describe "Option with value, with default" do
+
+    let(:args) do
+      args = ComposableArguments.new
+      args.add do |arg|
+        arg.key :foo
+        arg.default "default"
+        arg.on "--foo=VALUE", "--foo", "Set foo to VALUE"
+      end
+      args
+    end
+
+    it "prints help" do
+      test_harness.args = args
+      test_harness.parse!(["-h"])
+      expect(test_harness.output).to eq <<~OUTPUT
+        Usage: rspec [options]
+                --foo=VALUE                  Set foo to VALUE
+        (exit 0)
+      OUTPUT
+    end
+
+    it "is default before parsing" do
+      expect(args[:foo]).to eq "default"
+    end
+
+    it "is default after parsing when not set" do
+      args.parse!([])
+      expect(args[:foo]).to eq "default"
+    end
+
+    it "is the appropriate value when set" do
+      args.parse!(["--foo=BAR"])
+      expect(args[:foo]).to eq "BAR"
+    end
+    
   end
 
 end
