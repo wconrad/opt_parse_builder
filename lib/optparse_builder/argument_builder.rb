601fb282abe36c103c156138bca6f39e44b9aea0
Wayne Conrad <kf7qga@gmail.com>
2020-11-24 07:59:44 -0700

    More rdoc

--- lib/optparse_builder/argument_builder.rb
+++ lib/optparse_builder/argument_builder.rb
@@ -3,6 +3,115 @@ class OptparseBuilder
   # Builds arguments using a builder style DSL.  You never create an
   # instance of this class yourself.  Instead, an instance is yielded
   # to you by OptparseBuilder.
+  #
+  # # Types of arguments, by example
+  #
+  # A null argument, having no value or visible effect:
+  #
+  #     OptparseBuilder.build_argument do |arg|
+  #     end
+  #
+  # An argument with only banner text (but see OptparseBuilder#banner
+  # for the usual way to do this).  "Banner" is how OptParse describes
+  # text that appears at the top of the --help output.
+  #
+  #     OptparseBuilder.build_argument do |arg|
+  #       arg.banner "Some banner text"
+  #       arg.banner "A second line of banner text"
+  #       arg.banner <<~BANNER
+  #         A third line
+  #         A fourth line
+  #       BANNER
+  #     end
+  #
+  # An argument with only separator text (but see
+  # OptparseBuilder#banner for the usual way to do this).  "Separator"
+  # is how OptParse describes text that appears at the bottom of the
+  # --help output.
+  #
+  #     OptparseBuilder.build_argument do |arg|
+  #       arg.serparator "Separator text"
+  #       arg.serparator "A second line of separator text"
+  #       arg.serparator <<~SERPARATOR
+  #         A third line
+  #         A fourth line
+  #       SERPARATOR
+  #     end
+  #
+  # An argument with a constant value.  Perhaps of limited value, but
+  # it fell out of the design for free, so here it is:
+  #
+  #     OptparseBuilder.build_argument do |arg|
+  #       arg.key :limit
+  #       arg.default 12345
+  #     end
+  #
+  # A boolean option (switch) handled by OptParse:
+  #
+  #     OptparseBuilder.build_argument do |arg|
+  #       arg.key :quiet
+  #       arg.on "-q", "--quiet", "Suppress normal output"
+  #     end
+  #
+  #
+  # ## Value option
+  # 
+  # A value option is parsed by OptParse:
+  #
+  #     OptparseBuilder.build_argument do |arg|
+  #       arg.key :iterations
+  #       arg.default 100
+  #       arg.on "-i", "--iteraions=N",
+  #       arg.on "Number of iterations (default _DEFAULT_)"
+  #     end
+  #
+  # ## Required operand
+  #
+  # A required operand consumes one argument, with an error if there
+  # isn't one to consume.  
+  #
+  # This example overrides the help name, which is used to describe
+  # the operand in the --help text.  Optional and splat arguments can
+  # also have a help name override.
+  #
+  #     OptparseBuilder.build_argument do |arg|
+  #       arg.key :group
+  #       arg.required_operand help_name: "resource group"
+  #       arg.optional_operand
+  #     end
+  #
+  # The --help output for this argument looks like this:
+  #
+  #     myprogram [options] <resource group>
+  #
+  # ## Optional operand
+  #
+  # An optional operand consumes one argument.  If there isn't an
+  # argument to consume, then the value is either the default (if
+  # specified), or nil (if no default was specified).
+  #
+  #     OptparseBuilder.build_argument do |arg|
+  #       arg.key :group_name
+  #       arg.optional_operand
+  #     end
+  #
+  # The --help output for this argument looks like this:
+  #
+  #     myprogram [options] [<resource group>]
+  #
+  # ## Splat Operand
+  #
+  # A "splat" operand consumes all remaining arguments.  Its value is
+  # always an array.
+  #
+  #     OptparseBuilder.build_argument do |arg|
+  #       arg.key :input_path
+  #       arg.optional_operand
+  #     end
+  #
+  # The --help output for this argument looks like this:
+  #
+  #     myprogram [options] [<input path>...]
   class ArgumentBuilder
 
     def initialize # :nodoc:
